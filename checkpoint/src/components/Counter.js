f2516846989f5ce8496f2c3737efdb147947e0fd
Yassine <66576775+yassinedhieb@users.noreply.github.com>
2020-07-29 00:05:52 +0100

    first commit

---
+++ checkpoint/src/components/Counter.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { 
+    increaseCount,
+    decreaseCount
+} from '../Actions/Counter'
+
+class Counter extends Component {
+
+static mapStateToProps = state => {
+    return {
+        count: state.count
+    }
+}
+
+static mapDispatchToProps = dispatch => {
+    return bindActionCreators({
+        increaseCount,
+        decreaseCount
+    },
+    dispatch
+    )
+}
+
+render() {
+    const { increaseCount, decreaseCount } = this.props
+
+    return(
+        <div>
+        <button onClick={increaseCount} >+</button>
+        <span>{this.props.count}</span>
+        <button onClick={decreaseCount}>-</button>
+        </div>
+    )
+}
+
+}
+
+export default connect(
+    Counter.mapStateToProps,
+    Counter.mapDispatchToProps
+)(Counter)
